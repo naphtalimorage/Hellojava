52c0dd24177ed163aaec96f5c312b3d614f26881
Naphtali Morage <naphtalimorage56@gmail.com>
2024-06-14 18:57:51 +0300

    How to declare variables and initializing

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        //Variables
+        int num1= 2;
+        int num2= 4;
+        int result= num1 + num2;
+        System.out.println(result);
     }
 }
