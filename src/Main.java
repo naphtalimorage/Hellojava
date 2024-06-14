6ecc3acc4c20e8f97a2c0c2cc38537809d8f227e
Naphtali Morage <naphtalimorage56@gmail.com>
2024-06-14 19:49:45 +0300

    How to declare primitives datatypes

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,24 @@
 class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        //Variables
-        int num1= 2;
-        int num2= 4;
-        int result= num1 + num2;
-        System.out.println(result);
+        /**
+         * Variables
+         */
+//        int num1= 2;
+//        int num2= 4;
+//        int result= num1 + num2;
+//        System.out.println(result);
+
+        /**
+         * Datatype
+         */
+        byte num1= 127;
+        short num2 = 32668;
+        int num3 = 2147483548;
+        long num4 = 4000000000000l;
+        boolean on = true;
+        char grade = 'D';
+        float points = 5.6f;
+        double doublepoints = 5.7;
     }
 }
