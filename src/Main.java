8decf64c967ac3ff07c6a4e63df3ab9084fa4875
Naphtali Morage <naphtalimorage56@gmail.com>
2024-06-15 21:00:24 +0300

    how to work with relational operators

--- src/Main.java
+++ src/Main.java
@@ -10,17 +10,37 @@ class Hello {
 //        System.out.println(result);
 
 
+//        /**
+//         * Operators
+//         */
+//        int num1 = 25;
+//        int num2=5;
+//        int results1= num1 + num2; //addition operator
+//        int results2= num1 - num2; // minus operator
+//        int results3= num1 / num2; //division operator
+//        int results4= num1 % num2; //modulus operator
+//        int results5= num1++;    // post-increment operator
+//        int results6= ++num1;    // pre-increment operator
+//        System.out.println(results1);
+//        System.out.println(results2);
+//        System.out.println(results3);
+//        System.out.println(results4);
+//        System.out.println(results5);
+//        System.out.println(results6);
+
+
         /**
-         * Operators
+         * relational operators
          */
-        int num1 = 25;
-        int num2=5;
-        int results1= num1 + num2; //addition operator
-        int results2= num1 - num2; // minus operator
-        int results3= num1 / num2; //division operator
-        int results4= num1 % num2; //modulus operator
-        int results5= num1++;    // post-increment operator
-        int results6= ++num1;    // pre-increment operator
+
+        int num1= 12;
+        int num2=12;
+        boolean results1= num1 > num2;
+        boolean results2= num1 < num2;
+        boolean results3= num1 >= num2;
+        boolean results4= num1 <= num2;
+        boolean results5= num1 == num2;
+        boolean results6= num1 != num2;
         System.out.println(results1);
         System.out.println(results2);
         System.out.println(results3);
